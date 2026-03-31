24da24b8075dd5bc5f88d76509e9213c52fe97a0
jpasman82 <celularjpp@gmail.com>
2026-03-31 17:57:25 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
-import NewRotation from './pages/NewRotation';
 import EventDetail from './pages/EventDetail';
+import NewRotation from './pages/NewRotation';
+import BrokerDetail from './pages/BrokerDetail';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/rotaciones" element={<Dashboard />} />
         <Route path="/nuevo" element={<NewRotation />} />
         <Route path="/evento/:id" element={<EventDetail />} />
+        <Route path="/broker/:id" element={<BrokerDetail />} />
       </Routes>
     </Router>
   );
