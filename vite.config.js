6a1676827a2195d8653103e0231e0f972a7beb05
jpasman82 <celularjpp@gmail.com>
2026-04-01 14:21:40 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/byma': {
+        target: 'https://open.bymadata.com.ar/vanoms-be-core/rest/api/bymadata/free',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/byma/, '')
+      }
+    }
+  }
 })
