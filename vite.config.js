ea8c61f8de0adf89040aae3d73607b8522ab8c64
jpasman82 <celularjpp@gmail.com>
2026-04-01 14:30:03 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,9 @@ export default defineConfig({
         target: 'https://open.bymadata.com.ar',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => {
-          const match = path.match(/endpoint=([^&]+)/);
-          const endpoint = match ? match[1] : 'equities';
-          return `/vanoms-be-core/rest/api/bymadata/free/${endpoint}`;
-        },
+        rewrite: (path) => path.replace(/^\/api\/byma/, '/vanoms-be-core/rest/api/bymadata/free'),
         configure: (proxy) => {
           proxy.on('proxyReq', (proxyReq) => {
-            // Acá ocurre la magia: Le mentimos a BYMA diciéndole que somos ellos
             proxyReq.setHeader('Origin', 'https://open.bymadata.com.ar');
             proxyReq.setHeader('Referer', 'https://open.bymadata.com.ar/');
           });
