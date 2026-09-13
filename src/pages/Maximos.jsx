4b8d1f1ff22ad326fcdd5ebddd38e69fffdc137c
Juan <celularjpp@gmail.com>
2026-09-13 17:13:53 -0300

    fix: restore Maximos router link import

--- src/pages/Maximos.jsx
+++ src/pages/Maximos.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase/config';
 import AppBottomNav from '../components/AppBottomNav';
